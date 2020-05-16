ccfea96e8c84646d9f49f79dbb89b50bc0b5c488
Harshit Muhal <42070734+harshitmuhal@users.noreply.github.com>
2020-05-16 09:39:55 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,79 +1,106 @@
-#include<iostream>
-#include<math.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-bool canPlace(int board[][10],int i,int j,int n,int no){
-	//check for row and column
-	for(int x=0;x<n;x++){
-		if(board[x][j]==no or board[i][x]==no){
-			return false;
-		}
-	}
+int n;
+int sroot;
 
-	//check for subgrid
-	int rn = int(sqrt(n));	//nxn sudoku will contain sqrt(n) subgrids
-	int sx = (i/rn)*rn;
-	int sy = (j/rn)*rn;
+bool solve(int i, int j, vector<vector<char>>&board, int rows[], int column[], vector<vector<int>>&subgrid) {
+    if (i >= n || j >= n) {
+        return true;
+    }
+    if (board[i][j] == '0') {
+        for (int k = 1; k <= n; k++) {
+            int mask = 1 << (k - 1);
+            if (!(rows[i]&mask || column[j]&mask || subgrid[i / sroot][j / sroot]&mask)) {
+                rows[i] |= mask;
+                column[j] |= mask;
+                board[i][j] = k + '0';
+                //cout << board[i][j] << endl;
+                subgrid[i / sroot][j / sroot] |= mask;
+                bool issolved = false;
+                if (j == 8) {
+                    issolved = solve(i + 1, 0, board, rows, column, subgrid);
+                }
+                else {
+                    issolved = solve(i, j + 1, board, rows, column, subgrid);
+                }
+                if (issolved) {
+                    return true;
+                }
+                else {
+                    rows[i] &= (~mask);
+                    column[j] &= (~mask);
+                    subgrid[i / sroot][j / sroot] &= (~mask);
+                    board[i][j] = '0';
+                }
+            }
+        }
+        return false;
+    }
+    if (j == 8) {
+        return solve(i + 1, 0, board, rows, column, subgrid);
+    }
+    else {
+        return solve(i, j + 1, board, rows, column, subgrid);
+    }
+}
+void c_p_c() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int main() {
+    c_p_c();
+    cin >> n;
 
-	for(int x=sx;x<sx+rn;x++){
-		for(int y=sy;y<sy+rn;y++){
-			if(board[x][y]==no){
-				return false;
-			}
-		}
-	}
-	return true;
+    vector<vector<char>>board(n);
+    for (int i = 0; i < n; i++) {
+        board[i].resize(n, '0');
+    }
+
+    int rows[n];
+    int column[n];
+    fill(rows, rows + n, 0);
+    fill(column, column + n, 0);
+    vector<vector<int>>subgrid(n);
+    for (int i = 0; i < n; i++) {
+        subgrid[i].resize(n, 0);
+    }
+
+    sroot = sqrt(n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+            int mask = 1 << (board[i][j] - '1');
+            rows[i] |= mask;
+            column[j] |= mask;
+            subgrid[i / sroot][j / sroot] |= mask;
+        }
+    }
+
+    bool issolved = solve(0, 0, board, rows, column, subgrid);
+
+    if (issolved) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << board[i][j];
+            }
+            cout << endl;
+        }
+    }
+    else {
+        cout << "Invalid Grid\n";
+    }
+    return 0;
 }
 
 
 
-bool solveSudoku(int board[][10],int i,int j,int n){
-	//base case
-	if(i==n){
-		//print the board
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				cout<<board[i][j]<<" ";
-			}
-			cout<<endl;
-		}
-		cout<<endl;
-		return true;
-	}
-	if(j==n){
-		 return solveSudoku(board,i+1,0,n);
-	}
-	if(board[i][j]!=0){
-		return solveSudoku(board,i,j+1,n);
-	}
-	//rec case
-	for(int no=1;no<=n;no++){
-		if(canPlace(board,i,j,n,no)){
-			board[i][j] = no;
-			
-			bool aageKaSolveHua = solveSudoku(board,i,j+1,n);
-			if(aageKaSolveHua){
-				return true;
-			}
-		}
-	}
-	board[i][j] = 0; //backtracking
-	return false;
-}
 
 
-int main(){
 
-	int n;
-	cin>>n;
-	int board[10][10];
 
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cin>>board[i][j];
-		}
-	}
-	solveSudoku(board,0,0,n);
 
-	return 0;
-}
